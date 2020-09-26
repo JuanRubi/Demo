0ed96e3b7814123911fed8e2b89e2268709a30f4
Juan Rosales <rjuan361@gmail.com>
2020-09-26 01:30:30 -0700

    Added Form Processing
    The form is now process successfully using php to validate that the required fields are not empty.

---
+++ currentPortfolio/processContact.php
@@ -0,0 +1,26 @@
+
+<?php 
+    // Getting the user input.
+    $name = $_POST["name"];
+    $email = $_POST["mail"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    if($name != null && $email != null && $message != null)
+    {
+        $emailContent = "From: $name \n\n $message";
+        $sendTo = "juan@jrosales.tech";
+        $mailHeader = "From: $email \r\n";
+
+        // Sending the email to myself.
+        mail($sendTo, $subject, $emailContent, $mailHeader) or die("An error occurred!");
+        echo "<script> alert('Thank You!'); </script>";
+        echo "<script> window.history.back(); </script>";
+    }
+    else        // If required areas aren't filled in, then display alert.
+    {
+        // alert alternative? 
+        echo "<script> alert('Please fill in all required areas.'); </script>";
+        echo "<script> window.history.back(); </script>";
+    }
+?>
